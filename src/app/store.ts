1a154c9c99a4d15f0aba83c3cb55b722350aa589
danielislas3 <danielislas3@live.com>
2026-01-22 00:22:23 -0600

    feat: add redux store and provider

---
+++ src/app/store.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { ticketsApi } from '@/features/tickets/api/ticketsApi'
+
+export const store = configureStore({
+  reducer: {
+    [ticketsApi.reducerPath]: ticketsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(ticketsApi.middleware),
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
